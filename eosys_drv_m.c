09d8e29a1d1a36828430aa711e97f48917042e5f
yoonseok-seo <sysww@naver.com>
2025-07-14 22:59:11 +0900

    Update eosys_drv_m.c

--- eosys_drv_m.c
+++ eosys_drv_m.c
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2025 yoonseok seo
+ * Copyright (C) 2024 yoonseok seo
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
